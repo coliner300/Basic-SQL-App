bf153ec8c3e2018f8e250ff81161e06744d56dfc
coliner300 <coliner3000@gmail.com>
2021-01-11 22:15:30 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,21 @@
+.header on
+.mode column
+
+CREATE TABLE books
+(
+  title TEXT, 
+  author TEXT,
+  num_pages INTEGER,
+  rating DOUBLE, 
+  publication DATE
+);
+
+INSERT INTO books VALUES
+('Code Girls', 'Liza Mundy', 355, 4.8, 2017),
+('Calculus', 'Ron Larson', 1124, 0.5, 2006),
+('The Holy Bible', 'King James', 1200, 5, 140),
+('1984', 'George Orwell', 328, 5, 1949),
+('Crime and Punishment', 'Fyodor Dostoyvesky', 430, 4.0, 1866 );
+
+.print 'Average number of pages'
+SELECT avg(num_pages) AS avg_pages FROM books
